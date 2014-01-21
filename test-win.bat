caf5393faf0cd60a60db743e5511f7e9365cf26e
Stefanos A. <stapostol@gmail.com>
2014-01-21 11:32:32 +0100

    Improved windows test scripts

--- test-win.bat
+++ test-win.bat
@@ -1,9 +1,10 @@
-
-
 lib\managed\monolinker -a %1\bin\Release\%1.exe -l none -out %1\bin\Release\min\
-for %%i in (1,1,5) do (
+xcopy /y lib\x86\SDL2.dll %1\bin\Release\
+xcopy /y lib\x86\SDL2.dll %1\bin\Release\min\
+
+for %%i in (1,2,3,4,5) do (
  echo ===============
- echo %i
+ echo %%i
  del %1.mlpd
  %1\bin\Release\%1.exe
  dir %1\bin\Release\OpenTK.dll
@@ -12,9 +13,9 @@ for %%i in (1,1,5) do (
 echo ===============
 echo ===============
 echo ===============
-for %%i in (1,1,5) do (
+for %%i in (1,2,3,4,5) do (
  echo ===============
- echo %i
+ echo %%i
  del %1-min.mlpd
  %1\bin\Release\min\%1.exe
  dir %1\bin\Release\min\OpenTK.dll
--- win.bat
+++ win.bat
@@ -1,5 +1,6 @@
-test-win.bat test-opentk			> results-win-opentk.txt
-test-win.bat test-opentk-sdl2 			> results-win-opentk-sdl2.txt
-test-win.bat test-sdl2cs 			> results-win-sdl2cs.txt
-test-win.bat test-sdl2cs-opentk-external 	> results-win-sdl2cs-opentk.txt
-test-win.bat test-sdl2-opentk-external 		> results-win-sdl2-opentk.txt
+echo OFF
+CALL test-win.bat test-opentk			> results-win-opentk.txt
+CALL test-win.bat test-opentk-sdl2 		> results-win-opentk-sdl2.txt
+CALL test-win.bat test-sdl2cs 			> results-win-sdl2cs.txt
+CALL test-win.bat test-sdl2cs-opentk-external 	> results-win-sdl2cs-opentk.txt
+CALL test-win.bat test-sdl2-opentk-external	> results-win-sdl2-opentk.txt
