a7b531e914edd90c0bd936bb67bb8025ec5bf866
Stefanos A. <stapostol@gmail.com>
2014-01-21 11:05:08 +0100

    Added windows test scripts

---
+++ test-win.bat
@@ -0,0 +1,22 @@
+
+
+lib\managed\monolinker -a %1\bin\Release\%1.exe -l none -out %1\bin\Release\min\
+for %%i in (1,1,5) do (
+ echo ===============
+ echo %i
+ del %1.mlpd
+ %1\bin\Release\%1.exe
+ dir %1\bin\Release\OpenTK.dll
+ dir %1\bin\Release\SDL2-CS.dll
+)
+echo ===============
+echo ===============
+echo ===============
+for %%i in (1,1,5) do (
+ echo ===============
+ echo %i
+ del %1-min.mlpd
+ %1\bin\Release\min\%1.exe
+ dir %1\bin\Release\min\OpenTK.dll
+ dir %1\bin\Release\min\SDL2-CS.dll
+)
---
+++ win.bat
@@ -0,0 +1,5 @@
+test-win.bat test-opentk			> results-win-opentk.txt
+test-win.bat test-opentk-sdl2 			> results-win-opentk-sdl2.txt
+test-win.bat test-sdl2cs 			> results-win-sdl2cs.txt
+test-win.bat test-sdl2cs-opentk-external 	> results-win-sdl2cs-opentk.txt
+test-win.bat test-sdl2-opentk-external 		> results-win-sdl2-opentk.txt
